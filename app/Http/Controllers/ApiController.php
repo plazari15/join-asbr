05def90e81474c407a74fd03cb1aae19f0ccddd3
Pedro <plazari96@gmail.com>
2017-02-09 03:31:37 -0200

    Criando um método para remover pontos dependendo da região do usuário

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -37,6 +37,6 @@ class ApiController extends BaseController
             'unidade' => 'required|numeric',
         ]);
         $lead = new CreateLead($request);
-        dd();
+        dd($lead->getPointsForRegion());
     }
 }
--- app/Services/CreateLead.php
+++ app/Services/CreateLead.php
@@ -19,4 +19,37 @@ class CreateLead
 
 
     }
+
+    /**
+     * Este método vai informar ao sistema quantos pontos deve remover do lead por ser de uma determinada região
+     */
+    public function getPointsForRegion()
+    {
+        switch ($this->request->regiao){
+            case '1':
+                return '-2';
+                break;
+
+            case '2':
+                if($this->request->unidade == 3){
+                    return '0';
+                }
+                return '-1';
+                break;
+
+            case '3':
+                return '-3';
+                break;
+
+            case '4':
+                return '-4';
+                break;
+
+            case '5':
+                return '-5';
+                break;
+
+        }
+    }
+
 }
