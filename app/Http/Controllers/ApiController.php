72123c8223cf7f42566e6960ffb0f4d0f6fbfd56
Pedro <plazari96@gmail.com>
2017-02-09 01:36:42 -0200

    criando um controller para inserir a api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class IndexController extends BaseController
+{
+    /**
+     * Obtem as regiões do banco de dados
+     * @return \Illuminate\View\View
+     */
+    public function getRegion(Request $request){
+        //
+    }
+}
